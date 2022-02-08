3349ef3fc84ea4b8ed4c7f66cb36dbeacd0e3f19
Miyka96 <micaelaseminara@outlook.it>
2022-02-08 15:02:15 +0100

    creating variables

---
+++ main.js
@@ -0,0 +1,3 @@
+// CREO DELLE VARIABILI PER GLI ELEMENTI HTML SU CUI DEVO LAVORARE
+const hamburgerMenuList = document .getElementsByClassName('hamburger-menu');
+const hamburgerMenuIcon = document .getElementsByClassName('close');
