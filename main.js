191ee27549cec1f8b52bdc7e1409b8f56c6b75ec
Miyka96 <micaelaseminara@outlook.it>
2022-02-08 15:50:44 +0100

    finished

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 // CREO DELLE VARIABILI PER GLI ELEMENTI HTML SU CUI DEVO LAVORARE
-const hamburgerMenuList = document .getElementsByClassName('hamburger-menu');
-const hamburgerMenuIcon = document .getElementsByClassName('close');
+const hamburgerMenuList = document .querySelector('.hamburger-menu');
+const hamburgerMenuIcon = document .querySelector(".fas.fa-bars");
+const closeIcon = document .querySelector(".close")
+
+console.log(hamburgerMenuIcon)
+
+
+hamburgerMenuIcon.addEventListener('click', function () {
+    hamburgerMenuList.style.display= "block";
+})
+
+closeIcon.addEventListener('click', function () {
+    hamburgerMenuList.style.visibility= "hidden";
+})
+
